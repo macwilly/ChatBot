17fc4ccc5b3a5a69a75c0911e653c48bb11e81a7
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-12 15:03:09 -0500

    Updated ChatBot by adding in question bots logic

--- ChatBot/ConversationDelegate.swift
+++ ChatBot/ConversationDelegate.swift
@@ -1,23 +1,33 @@
 struct ConversationDelegate {
     /// Creates a String in response to another String.
     func responseTo(question: String) -> String {
-        let lowerQuestion = question.lowercased()
+        let lowerQuestion = question.lowercased();
         
-        if lowerQuestion.hasPrefix("hello") {
-            return "Why, hello there!"
-        } else if lowerQuestion == "where are the cookies?" {
+        if lowerQuestion == "where are the cookies?" {
             return "In the cookie jar!"
+        } else if lowerQuestion == ("hello there!") {
+            return "General Kenobi!!!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there!"
         } else if lowerQuestion.hasPrefix("where") {
             return "To the North!"
+        } else if lowerQuestion.hasPrefix("who") {
+            return "Not sure just became sentient..."
+        } else if lowerQuestion.hasPrefix("what") {
+            return "The answer is 42 you are just not asking the correct question!"
+        } else if lowerQuestion.hasPrefix("why") {
+            return "I'll do you one better why is Gimora!"
+        } else if lowerQuestion.hasPrefix("how") {
+            return "How?!  How dare you!!"
         } else {
             let defaultNumber = question.count % 3
             
             if defaultNumber == 0 {
                 return "That really depends"
             } else if defaultNumber == 1 {
-                return "I think so, yes"
-            } else {
                 return "Ask me again tomorrow"
+            } else {
+                return "not all questions have answers."
             }
         }
     }
